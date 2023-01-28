83ebc7d68fd9e7c30e8a465865a8ab32c5ac7ae1
Brandon Moye <brandon.m.moye@gmail.com>
2023-01-28 17:57:58 -0600

    can get links in objects

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const seasonArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
 let seasonsWebpageArray = [];
 let masterDatabase = new Object();
+let seasonDatabase = new Object();
+let queenLinkDatabase = new Object();
 for (let i = 0; i < seasonArray.length; i++) {
   // console.log(seasonArray[i]);
   seasonsWebpageArray.push(
@@ -21,20 +23,25 @@ for (let i = 0; i < seasonArray.length; i++) {
   masterDatabase.webpage = seasonsWebpageArray;
 }
 console.log(masterDatabase);
-
+queenLinks = [];
 for (let i = 0; i < seasonsWebpageArray.length; i++) {
   axios.get(seasonsWebpageArray[i]).then((res) => {
-    const maximumCast = 17;
+    const maximumCast = 20;
+    seasonDatabase.season = seasonsWebpageArray[i];
+    console.log(seasonDatabase);
     for (let i = 0; i < maximumCast; i++) {
       const $ = cheerio.load(res.data);
-      let nameOfQueen = $(
-        `table[class="wikitable"] tbody tr:nth-child(${i}) td:nth-child(2) b a`
-      ).text();
-      if (nameOfQueen === undefined) {
-        nameOfQueen = $(
-          `table[class="wikitable"] tbody tr:nth-child(${i}) td:nth-child(1) b a`
-        ).text();
-      }
+      // let nameOfQueen = $(
+      //   `table[class="wikitable"] tbody tr:nth-child(${i}) td:nth-child(2) b a`
+      // ).text();
+
+      // if (nameOfQueen === undefined) {
+      //   nameOfQueen = $(
+      //     `table[class="wikitable"] tbody tr:nth-child(${i}) td:nth-child(1) b a`
+      //   ).text();
+      // }
+      // console.log(nameOfQueen);
+
       let linkToQueenPage = $(
         `table[class="wikitable"] tbody tr:nth-child(${i}) td:nth-child(2) b a`
       ).attr("href");
@@ -42,10 +49,15 @@ for (let i = 0; i < seasonsWebpageArray.length; i++) {
         linkToQueenPage = $(
           `table[class="wikitable"] tbody tr:nth-child(${i}) td:nth-child(1) b a`
         ).attr("href");
-        console.log(linkToQueenPage);
-        console.log(nameOfQueen);
       }
+      // console.log(linkToQueenPage);
+      queenLinks.push(linkToQueenPage);
+      // console.log(queenLinks);
+      queenLinkDatabase.queenLinks = linkToQueenPage;
+      console.log(queenLinkDatabase);
     }
+    // console.log(seasonDatabase);
+    // queenLinks.push(linkToQueenPage);
   });
 }
 
@@ -60,7 +72,7 @@ for (let i = 0; i < seasonsWebpageArray.length; i++) {
 // }
 console.log("end of test");
 const rpdrS1URL =
-  "https://rupaulsdragrace.fandom.com/wiki/RuPaul%27s_Drag_Race_(Season_4)";
+  "https://rupaulsdragrace.fandom.com/wiki/RuPaul%27s_Drag_Race_(Season_8)";
 
 axios.get(rpdrS1URL).then((res) => {
   const $ = cheerio.load(res.data);
@@ -69,7 +81,7 @@ axios.get(rpdrS1URL).then((res) => {
   //   $(`table[class="wikitable"] tbody tr:last-child td:first-child`).html(),
   // ]);
 
-  queenIndexNumber = 5;
+  queenIndexNumber = 6;
 
   let trialForQueenURL = $(
     `table[class="wikitable"] tbody tr:nth-child(${queenIndexNumber}) td:nth-child(2) b a`
@@ -78,17 +90,17 @@ axios.get(rpdrS1URL).then((res) => {
     `table[class="wikitable"] tbody tr:nth-child(${queenIndexNumber}) td:nth-child(2) b a`
   ).text();
 
-  if (trialForQueenURL === undefined || trialForQUeenName === undefined) {
+  if (trialForQueenURL === undefined) {
     trialForQueenURL = $(
-      `table[class="wikitable"] tbody tr:nth-child(${queenIndexNumber}) td:nth-child(1) b a`
+      `table[class="wikitable"] tbody tr:nth-child(${queenIndexNumber}) td:nth-child(2) a`
     ).attr("href");
-    trialForQUeenName = $(
-      `table[class="wikitable"] tbody tr:nth-child(${queenIndexNumber}) td:nth-child(1) b a`
-    ).text();
+    // trialForQUeenName = $(
+    //   `table[class="wikitable"] tbody tr:nth-child(${queenIndexNumber}) td:nth-child(1) b a`
+    // ).text();
   }
-  console.log(trialForQueenURL);
-  console.log(trialForQUeenName);
-  console.log();
+  // console.log(trialForQueenURL);
+  // console.log(trialForQUeenName);
+  // console.log();
 });
 
 // DONT TOUCH - THIS PATH GETS ME TO AKASHIA
