e615a97161cb13376932c741bc9a84f7dd49db8b
Brandon Moye <brandon.m.moye@gmail.com>
2023-01-28 13:48:04 -0600

    creating an array of each webpage to use

--- index.js
+++ index.js
@@ -7,60 +7,33 @@ const express = require("express");
 
 const app = express();
 
-const url = "https://www.theguardian.com/uk";
-
-//kind of works for the guardian
-axios(url)
-  .then((response) => {
-    const html = response.data;
-    // console.log(html);
-    const $ = cheerio.load(html);
-    const articles = [];
-
-    $(".fc-item__content").each(function () {
-      const title = $(this).text();
-      const url = $(this).find("a").attr("href");
-      const actualTitle = $(this).find("span").text();
-      articles.push({
-        title,
-        // url,
-        // actualTitle,
-      });
-    });
-    // console.log(articles);
-  })
-  .catch((error) => console.log(error));
-
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
 
-const rpdrURL =
+const seasonArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+let seasonsWebpageArray = [];
+for (let i = 0; i < seasonArray.length; i++) {
+  // console.log(seasonArray[i]);
+  seasonsWebpageArray.push(
+    `https://rupaulsdragrace.fandom.com/wiki/RuPaul%27s_Drag_Race_(Season_${seasonArray[i]})`
+  );
+}
+for (let i = 0; i < seasonArray.length; i++) {
+  let rpdrURLBeingUsed = seasonsWebpageArray[i];
+  console.log(rpdrURLBeingUsed);
+}
+console.log("end of test");
+
+const rpdrS1URL =
   "https://rupaulsdragrace.fandom.com/wiki/RuPaul%27s_Drag_Race_(Season_1)";
 
-axios.get(rpdrURL).then((res) => {
+axios.get(rpdrS1URL).then((res) => {
   const $ = cheerio.load(res.data);
-  $(".mw-parser-output")
-    .children("center")
-    .each((index, element) => {
-      // console.log(
-      //   $(element)
-      //     // .children("center")
-      //     .children("table")
-      //     .children("tbody")
-      //     .children("tr")
-      //     .children("td")
-      //     .children("div")
-      //     .children("div")
-      //     .children("a")
-      //     .attr("href")
-      //   // .html()
-      // );
-    });
-  const gettingToSpecificTablePathway = $(".mw-parser-output")
-    .children(".wikitable")
-    .children("tbody");
-  // .children()
-  // .html();
-  // console.log(gettingToSpecificTablePathway);
+
+  console.log([
+    $(`table[class="wikitable"] tbody tr:last-child td:first-child`).html(),
+  ]);
+
   queenIndexNumber = 3;
   console.log(
     $(
@@ -72,19 +45,6 @@ axios.get(rpdrURL).then((res) => {
       `table[class="wikitable"] tbody tr:nth-child(${queenIndexNumber}) td:nth-child(2) b a`
     ).text()
   );
-
-  // console.log(
-  //   $(".mw-parser-output")
-  //     .children("center")
-  //     .children("table")
-  //     .children("tbody")
-  //     .children("tr")
-  //     .children("td")
-  //     .children("div")
-  //     .children("div")
-  //     .children("a")
-  //     .attr("title")
-  // );
 });
 
 // DONT TOUCH - THIS PATH GETS ME TO AKASHIA
